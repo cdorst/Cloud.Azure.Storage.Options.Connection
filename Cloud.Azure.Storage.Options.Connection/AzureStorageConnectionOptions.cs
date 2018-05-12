91e4156ee7d20c3113fd15ffb39474e6b8bcb79d
Christopher Dorst <chris@dorst.email>
2018-05-12 04:25:56 -0500

    Add AzureStorageConnectionOptions.cs

---
+++ Cloud.Azure.Storage.Options.Connection/AzureStorageConnectionOptions.cs
@@ -0,0 +1,12 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
+namespace Cloud.Azure.Storage.Options.Connection
+{
+    /// <summary>Azure Storage connection string options</summary>
+    public class AzureStorageConnectionOptions
+    {
+        /// <summary>Contains the Azure Storage service connection string</summary>
+        public string ConnectionString { get; set; }
+    }
+}
